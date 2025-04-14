3da034e54c9e47abf3723b841fb48e074dd8b97f
alikasrawy <kasrawyali47@gmail.com>
2025-04-15 00:44:41 +0200

    add container background to start view

--- lib/Featured/Start/Views/start_view.dart
+++ lib/Featured/Start/Views/start_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/Core/utils/assets.dart';
+import 'package:quiz_app/Featured/Start/Widgets/start_view_body.dart';
 
 class StartView extends StatelessWidget {
   const StartView({super.key});
   static String id = "StartView";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.imagesLoginScreen),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: StartViewBody(),
+      ),
+    );
   }
 }
--- lib/constents.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/material.dart';
-// import 'package:quiz_app/Core/utils/assets.dart';
-// import 'package:quiz_app/Featured/on_boarding/Models/item_model.dart';
-
-// abstract class Constents {
-//   static 
-// //  static double currentIndex = 0;
-// }
