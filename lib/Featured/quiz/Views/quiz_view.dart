03de251b16e28e06aeb1a3ac899eea952bd897ac
alikasrawy <kasrawyali47@gmail.com>
2025-04-16 00:38:39 +0200

    create quiz view body

--- lib/Featured/quiz/Views/quiz_view.dart
+++ lib/Featured/quiz/Views/quiz_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/Featured/quiz/Widgets/custom_app_bar_quiz_view.dart';
+import 'package:quiz_app/Featured/quiz/Widgets/quiz_view_body.dart';
 
 class QuizView extends StatelessWidget {
   const QuizView({super.key});
@@ -10,6 +11,7 @@ class QuizView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffEFF0F3),
       appBar: CustomAppBarQuizView(),
+      body: QuizViewBody(),
     );
   }
 }
